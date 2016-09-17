1b88534a7a7ad1f0f09206c6beaaaf5210bcd079
Gauri <Gauri@Gauris-MBP.wv.cc.cmu.edu>
2016-09-17 10:58:54 -0400

    fixed toggle

--- static_site/js/toggling.js
+++ static_site/js/toggling.js
@@ -1,4 +1,4 @@
-$(".answer_btn").onclick = doFunction;
+/*$(".answer_btn").onclick = doFunction;
 
 $(".answer_btn").onclick = function(){
 	$(this).sub
@@ -8,4 +8,22 @@ $(document).ready(function() {
   $(".answer_btn").click(function() {     
     $(".question_area").slideToggle();
   });
-});
+});*/
+
+
+<!--
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+//-->
+
+
+/* $(document).ready(function() { 
+  $(".entry-toggle").click(function() {     
+    $(this).siblings(".entry-content").slideToggle();
+  });
+}); */
