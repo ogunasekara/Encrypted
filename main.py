cf046779cd1a71898f423b305b89c2623ee6067b
Oshadha Gunasekara <ovg@andrew.cmu.edu>
2016-09-16 23:32:09 -0700

    added static site template & bootstrap

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 # app configuration
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/webdevblog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///encrmsg.db"
 app.debug = False
 
 # db configuration
@@ -12,31 +12,31 @@ db = SQLAlchemy(app)
 
 class BlogEntry(db.Model):
   ID = db.Column(db.Integer, primary_key=True)
-  author = db.Column(db.String(50))
-  title = db.Column(db.String(100))
+  header = db.Column(db.String(50))
+  author = db.Column(db.String(30))
   text = db.Column(db.String(5000))
   date = db.Column(db.DateTime())
 
-  def __init__(self, author, title, text):
+  def __init__(self, header, author, text):
+    self.header = header
     self.author = author
-    self.title = title
     self.text = text
     self.date = datetime.utcnow()
 
   def __repr__(self):
-    return "POST %s by %s on %s" %(self.title, self.author, self.date)
+    return "POST %s by %s on %s" %(self.header, self.author, self.date)
 
 db.create_all()
 
 # helper functions
-def createPost(author, title, text):
+def createPost(author, header, text):
   '''
   Creates a BlogEntry if given valid parameters, else return None.
   '''
   # first verify components
-  if author and title and text and \
-      len(author) <= 50 and len(title) <= 100 and len(text) <= 5000:
-    return BlogEntry(author, title, text)
+  if author and header and text and \
+      len(author) <= 30 and len(header) <= 50 and len(text) <= 5000:
+    return BlogEntry(author, header, text)
   return None
 
 # routing
@@ -57,7 +57,7 @@ def create():
 @app.route("/newpost", methods=["POST"])
 def newpost():
   author = request.form["post-name"]
-  title = request.form["post-title"]
+  title = request.form["post-header"]
   text = request.form["post-text"]
   post = createPost(author, title, text)
   # case invalid
